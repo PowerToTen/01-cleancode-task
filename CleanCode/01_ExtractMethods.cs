79160a684a83cfc9e3e43652044318a1081e63ca
PowerToTen <eugene.ostanin@gmail.com>
2014-11-28 15:48:57 +0500

    a111d

--- CleanCode/01_ExtractMethods.cs
+++ CleanCode/01_ExtractMethods.cs
@@ -1,30 +1,52 @@
-﻿using System;
+﻿using System;   
 using System.IO;
 
 namespace CleanCode
 {
 	public static class RefactorMethod
 	{
-		private static void SaveData(string s, byte[] d)
+		private static void SaveData(string filePath, byte[] dataToWtiteBytes)
 		{
 			//open files
-			var fs1 = new FileStream(s, FileMode.OpenOrCreate);
-			var fs2 = new FileStream(Path.ChangeExtension(s, "bkp"), FileMode.OpenOrCreate);
+		    FileStream fileStreamBackup;
+		    var fileStream = OpenFileStream(filePath, out fileStreamBackup);
 
-			// write data
-			fs1.Write(d, 0, d.Length);
-			fs2.Write(d, 0, d.Length);
+		    // write data
+			WriteDataToFile(dataToWtiteBytes, fileStream, fileStreamBackup);
 
-			// close files
-			fs1.Close();
-			fs2.Close();
+		    // close files
+			CloseFiles(fileStream, fileStreamBackup);
 
-			// save last-write time
-			string tf = s + ".time";
-			var fs3 = new FileStream(tf, FileMode.OpenOrCreate);
-			var t = BitConverter.GetBytes(DateTime.Now.Ticks);
-			fs3.Write(t, 0, t.Length);
-			fs3.Close();
+		    // save last-write time
+			SaveLastWriteTime(filePath);
 		}
+
+	    private static FileStream OpenFileStream(string filePath, out FileStream fileStreamBackup)
+	    {
+	        var fileStream = new FileStream(filePath, FileMode.OpenOrCreate);
+	        fileStreamBackup = new FileStream(Path.ChangeExtension(filePath, "bkp"), FileMode.OpenOrCreate);
+	        return fileStream;
+	    }
+
+	    private static void SaveLastWriteTime(string filePath)
+	    {
+	        var timeFilePath = filePath + ".time";
+	        var fileStream = new FileStream(timeFilePath, FileMode.OpenOrCreate);
+	        var t = BitConverter.GetBytes(DateTime.Now.Ticks);
+	        fileStream.Write(t, 0, t.Length);
+	        fileStream.Close();
+	    }
+
+	    private static void CloseFiles(FileStream fileStream, FileStream fileStreamBackup)
+	    {
+	        fileStream.Close();
+	        fileStreamBackup.Close();
+	    }
+
+	    private static void WriteDataToFile(byte[] dataToWtiteBytes, FileStream fileStream, FileStream fileStreamBackup)
+	    {
+	        fileStream.Write(dataToWtiteBytes, 0, dataToWtiteBytes.Length);
+	        fileStreamBackup.Write(dataToWtiteBytes, 0, dataToWtiteBytes.Length);
+	    }
 	}
 }
